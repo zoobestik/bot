0f7341b620d629ecdcbccae8d77f6c0690322069
zoobestik <kb.chernenko@gmail.com>
2017-01-24 20:30:34 +0300

    common; upgrade telegrambots API to 2.4.4

--- src/main/java/me/telegram/borken_bot/App.java
+++ src/main/java/me/telegram/borken_bot/App.java
@@ -1,12 +1,13 @@
 package me.telegram.borken_bot;
 
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 
 public class App {
     public static void main(String[] args) {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-
         try {
+            ApiContextInitializer.init();
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
             telegramBotsApi.registerBot(new GMBot());
         } catch (Exception e) {
             e.printStackTrace();
