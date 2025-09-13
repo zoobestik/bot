5b01793a25b2dffcaf5d8ebbef79e803cb598f14
zoobestik <kb.chernenko@gmail.com>
2025-09-13 22:25:48 +0200

    Add multi-platform support to a Docker image build script

--- scripts/docker-image.sh
+++ scripts/docker-image.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 BOT_VERSION=$(grep -m1 "^version = " Cargo.toml | cut -d'"' -f2)
 # BOT_VERSION=$(cargo metadata --no-deps --format-version=1 | jq -r .packages[0].version)
 
-docker build -t "zoobestik/gamebot:$BOT_VERSION" .
+docker build --platform linux/amd64,linux/arm64 -t "zoobestik/gamebot:$BOT_VERSION" .
